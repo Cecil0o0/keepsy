7c0067ac0660100fe27e8c12d96a4b2e5957335e
haojie <jiegithub@gmail.com>
2025-05-05 22:14:02 +0800

    refactor for a new scan fn for being more like wikipedia design.

--- src/tokenizer.zig
+++ src/tokenizer.zig
@@ -224,15 +224,19 @@ const stderr = std.io.getStdErr().writer();
 const TokenizeResult = struct { original: []const u8, evaluations: std.ArrayList(EvaluateResult) };
 
 pub fn tokenize(string: []const u8) !TokenizeResult {
+    const evaluations = try scan(string);
+    return TokenizeResult{ .original = string, .evaluations = evaluations };
+}
+
+fn scan(string: []const u8) !std.ArrayList(EvaluateResult) {
     var select_dfa = SelectDFA{ .state = State.NULL };
     var from_dfa = FromDFA{};
     var star_dfa = StarDFA{};
     var double_quote_dfa = DoubleQuoteStringDFA{};
     var column_dfa = ColumnDFA{};
     var table_dfa = TableDFA{};
-    var tokenize_result = TokenizeResult{ .original = string, .evaluations = std.ArrayList(EvaluateResult).init(std.heap.page_allocator) };
     var i: u32 = 0;
-    std.debug.print("input string: {s}", .{string});
+    var evaluations = std.ArrayList(EvaluateResult).init(std.heap.page_allocator);
     while (i < string.len) {
         const c = string[i];
         std.debug.print("\ntry to dispatch dfa '{c}'", .{c});
@@ -254,7 +258,7 @@ pub fn tokenize(string: []const u8) !TokenizeResult {
             } else {
                 select_dfa.col[1] = j;
                 std.debug.print("\nselect_dfa accepted: \x1B[32m{s}\x1B[0m", .{select_dfa.value});
-                try tokenize_result.evaluations.append(evaluate(DFA{ .select = select_dfa }));
+                try evaluations.append(evaluate(DFA{ .select = select_dfa }));
             }
 
             // suspect `regular column` segment, could be extended to `function-call column`.
@@ -265,7 +269,7 @@ pub fn tokenize(string: []const u8) !TokenizeResult {
                 i += 1;
                 star_dfa.col[1] = i;
                 std.debug.print("\nstar_dfa accepted: \x1B[32m{s}\x1B[0m", .{star_dfa.value});
-                try tokenize_result.evaluations.append(evaluate(DFA{ .star = star_dfa }));
+                try evaluations.append(evaluate(DFA{ .star = star_dfa }));
             } else {
                 // forward four letter
                 while (string[i] != 'f' or string[i + 1] != 'r' or string[i + 2] != 'o' or string[i + 3] != 'm') {
@@ -284,7 +288,7 @@ pub fn tokenize(string: []const u8) !TokenizeResult {
                         } else {
                             column_dfa.col[1] = k;
                             std.debug.print("\ncolumn_dfa accepted: \x1B[32m{s}\x1B[0m", .{column_dfa.value.items});
-                            try tokenize_result.evaluations.append(evaluate(DFA{ .column = column_dfa }));
+                            try evaluations.append(evaluate(DFA{ .column = column_dfa }));
                             column_dfa.value.clearAndFree();
                         }
                     }
@@ -308,7 +312,7 @@ pub fn tokenize(string: []const u8) !TokenizeResult {
                 } else {
                     from_dfa.col[1] = j;
                     std.debug.print("\nfrom_dfa accepted: \x1B[32m{s}\x1B[0m", .{from_dfa.value});
-                    try tokenize_result.evaluations.append(evaluate(DFA{ .from = from_dfa }));
+                    try evaluations.append(evaluate(DFA{ .from = from_dfa }));
                 }
             }
 
@@ -328,7 +332,7 @@ pub fn tokenize(string: []const u8) !TokenizeResult {
                 } else {
                     table_dfa.col[1] = k;
                     std.debug.print("\ntable_dfa accepted: \x1B[32m{s}\x1B[0m", .{table_dfa.value.items});
-                    try tokenize_result.evaluations.append(evaluate(DFA{ .table = table_dfa }));
+                    try evaluations.append(evaluate(DFA{ .table = table_dfa }));
                 }
             }
         } else if (try double_quote_dfa.transition(c) == State.START) {
@@ -346,7 +350,7 @@ pub fn tokenize(string: []const u8) !TokenizeResult {
             } else {
                 double_quote_dfa.col[1] = j;
                 std.debug.print("\ndouble_quote_dfa accepted: \x1B[32m{s}\x1B[0m", .{double_quote_dfa.value.items});
-                try tokenize_result.evaluations.append(evaluate(DFA{ .double_quote_string = double_quote_dfa }));
+                try evaluations.append(evaluate(DFA{ .double_quote_string = double_quote_dfa }));
             }
         } else if (c == ' ') {
             std.debug.print(" Leading whitespace character will be ignored for now.", .{});
@@ -357,7 +361,7 @@ pub fn tokenize(string: []const u8) !TokenizeResult {
         }
     }
     std.debug.print("\n", .{});
-    return tokenize_result;
+    return evaluations;
 }
 
 test "A incorrect keyword in DQL" {
