9c4c2567d9f09717431c86bb6bf73af117985e06
haojie <jiegithub@gmail.com>
2026-02-23 22:21:01 +0800

    support a binary for CLI.

--- nativo/src/main.zig
+++ nativo/src/main.zig
@@ -2,5 +2,23 @@ const std = @import("std");
 const nativo = @import("nativo");
 
 pub fn main() !void {
-    std.debug.print("Hello World!", .{});
+    const allocator = std.heap.page_allocator;
+    const args = try std.process.argsAlloc(allocator);
+    if (args.len < 2) {
+        std.debug.print("Usage: {s} <html_file_path>\n", .{args[0]});
+        return error.MissingArgument;
+    }
+    defer std.process.argsFree(allocator, args);
+    std.debug.print("html {s}\n", .{args[1]});
+    const manifest = try nativo.get_manifest_from_html(
+        allocator,
+        std.fs.cwd().readFileAlloc(allocator, args[1], 4096) catch |err| {
+            std.debug.print("Error: {any}\n", .{err});
+            return;
+        },
+    );
+    std.debug.print("\n", .{});
+    for (manifest) |module| {
+        std.debug.print("module {s}\n", .{module});
+    }
 }
