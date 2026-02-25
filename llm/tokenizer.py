c78ad58a428f8a8417dc7682875d7bf8d2a4e641
haojie <jiegithub@gmail.com>
2026-02-25 19:32:23 +0800

    distinguish between embedding and tokens.

--- llm/tokenizer.py
+++ llm/tokenizer.py
@@ -4,5 +4,7 @@ enc = tiktoken.get_encoding("cl100k_base")
 with open('control-plane.txt', 'r') as f:
     content = f.read()
     encoded_content = enc.encode(content)
-    print(encoded_content  )
-    print(enc.decode(encoded_content))
+    print(encoded_content)
+    print("Does the decoded content match the original content?", enc.decode(encoded_content) == content)
+
+print(enc.encode("香蕉"), enc.encode("水果"), enc.encode("卡车"))
